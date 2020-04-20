b48775b6ce61c41a7634429247c414e755fd1573
abialkowska <abialkowska247@gmail.com>
2020-04-20 20:09:53 +0200

    add trello classes

--- src/main/java/com/crud/tasks/TaskApplication.java
+++ src/main/java/com/crud/tasks/TaskApplication.java
@@ -16,4 +16,8 @@ public class TaskApplication extends SpringBootServletInitializer {
 //    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 //        return application.sources(TaskApplication.class);
 //    }
+
+    //2047370d9ab0fd85d6ffd6f9b82f68d6
+//d523fda7b0fed53ab8ff9eab05a20ac49f11d7fd432080356ce284e51a50c4c6
+
 }
