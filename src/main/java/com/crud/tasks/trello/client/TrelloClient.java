bf62a1bbb91e6503b33648aee6aca0855252395e
abialkowska <abialkowska247@gmail.com>
2020-04-22 21:33:50 +0200

    correct values in annotations, add private method

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -15,26 +15,35 @@ import java.util.List;
 
 @Component
 public class TrelloClient {
-    @Value("trello.api.endpoint.prod")
+    @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
-    @Value("trello.app.key")
+    @Value("${trello.app.key}")
     private String trelloAppKey;
-    @Value("trello.app.token")
+    @Value("${trello.app.token}")
     private String trelloToken;
+    @Value("${trello.app.username}")
+    private String username;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public List<TrelloBoardDto> getTrelloBoards() {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/kodillauser/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken).build().encode().toUri();
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(buildUri(username),
+                                                                    TrelloBoardDto[].class);
 
         if (boardsResponse != null) {
             return Arrays.asList(boardsResponse);
         }
         return new ArrayList<>();
+    }
+
+    private URI buildUri(String user) {
+        return UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + user + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                //.queryParam("lists", "all")
+                .build().encode().toUri();
     }
 }
